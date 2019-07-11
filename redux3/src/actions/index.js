32115833042c48bec0937668096e3bd66f92be31
aakashDeveloper <ahanda205@gmail.com>
2019-07-11 04:07:05 +0100

    a

---
+++ redux3/src/actions/index.js
@@ -0,0 +1,20 @@
+import { getPropertyData } from './propertSearch';
+
+export function openHouseData(response){
+    return { type: 'GET_OPENHOUSE_DATA', payload:response}
+}
+
+const getPropertyList = () => async(dispatch) => {
+    try{
+        const response = await getPropertyData();
+        dispatch(openHouseData(response))
+    } catch(err){
+        dispatch(openHouseData([]))
+    }
+}
+
+
+export const fetchProperty ={
+    openHouseData,
+    getPropertyList
+}
---
+++ redux3/src/actions/propertSearch.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { API } from '../config';
+
+export function getPropertyData(){
+    const config = {
+        url:`${API.API_LINK}?limit=5`,
+        method:'GET',
+        headers:{
+            'Authorization':'Basic c2ltcGx5cmV0czpzaW1wbHlyZXRz',
+            'Content-Type':'application/json'
+        }
+    };
+
+    var output = axios.request(config)
+        .then((response) =>{
+            return response.data
+        });
+    return output;
+}   
